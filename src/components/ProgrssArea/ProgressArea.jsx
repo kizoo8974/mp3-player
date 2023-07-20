778f6ebc483ecadbe3dc15a62813e00124ba5cc3
Joon Kim <kizoo8974@gamil.com>
2023-07-20 12:39:20 +0900

    Progress bar

--- src/components/ProgrssArea/ProgressArea.jsx
+++ src/components/ProgrssArea/ProgressArea.jsx
@@ -1,4 +1,4 @@
-import React, {useRef, useImperativeHandle, forwardRef } from "react";
+import React, {useRef, useImperativeHandle, forwardRef, useState } from "react";
 import "./ProgressArea.scss";
 import music1 from "../../music/music-1.mp3";
 import { useDispatch } from "react-redux";
@@ -6,7 +6,10 @@ import { playMusic, stopMusic } from "../../store/musicPlayerReducer";
 
 function ProgressArea(props, ref) {
   const audio = useRef()
+  const progressBar = useRef()
   const dispatch = useDispatch()
+  const [currentTime, setcurrentTime] = useState("00:00")
+  const [duration, setduration] = useState("00:00")
 
   useImperativeHandle(ref, ()=>({
     play:()=>{
@@ -21,24 +24,48 @@ function ProgressArea(props, ref) {
     dispatch(playMusic())
   }
 
+  const getTime = (time) => {
+    const minute = `0${parseInt(time/60,10)}`
+    const seconds = `0${parseInt(time%60)}`
+    return `${minute}:${seconds.slice(-2)}`
+  }
+
+  const onClickProgress = (event) => {
+    const progressBarWidth = event.currentTarget.clientWidth;
+    const offsetX = event.nativeEvent.offsetX;
+    const duration = audio.current.duration;
+    audio.current.currentTime = (offsetX/progressBarWidth) * duration
+  }
+
+  const onTimeUpdate = (event) => {
+    if(event.target.readyState === 0) return;
+    const currentTime = event.target.currentTime;
+    const duration = event.target.duration;
+    const progressBarWidth = (currentTime/duration) * 100;
+    progressBar.current.style.width = `${progressBarWidth}%`;
+    setcurrentTime(getTime(currentTime))
+    setduration(getTime(duration))
+  }
+
   const onPause = () => {
     dispatch(stopMusic())
   }
 
   return (
-    <div className="progress-area">
-      <div className="progress-bar">
+    <div className="progress-area" onMouseDown={onClickProgress}>
+      <div className="progress-bar" ref={progressBar}>
         <audio
           autoPlay
           ref={audio}
           src={music1}
           onPlay={onPlay}
           onPause={onPause}
+          onTimeUpdate={onTimeUpdate}
         ></audio>
       </div>
       <div className="music-timer">
-        <span>00:00</span>
-        <span>00:00</span>
+        <span>{currentTime}</span>
+        <span>{duration}</span>
       </div>
     </div>
   );
